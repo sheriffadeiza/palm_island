4656249a0fe1a5333f9cae9a920e1ede344bc412
sheriff adeiza <sheriffadeiza10@gmail.com>
2025-08-25 02:40:44 -0700

    use_cors

--- bid_backend/index.js
+++ bid_backend/index.js
@@ -68,10 +68,14 @@ const app = express();
 
 
 app.use(cors({
-  origin: [process.env.FRONTEND_URL, "http://localhost:5173"],
+  origin: [
+    "http://localhost:5173", 
+    "https://project-neon-rho.vercel.app"
+  ],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
+
 
 app.use(json());
 
